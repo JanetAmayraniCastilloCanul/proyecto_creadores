a800ae3afc43376c630b0c00ce1ddfe28aad41c4
AMY CASTILLO <AMY CASTILLO@JanetCastillo>
2020-05-10 14:51:44 -0500

    He creado la tabla de productos

---
+++ proyecto_creadores/src/main/java/com/nfsm/springboot/web/app/ProyectoCreadoresApplication.java
@@ -0,0 +1,13 @@
+package com.nfsm.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCreadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCreadoresApplication.class, args);
+	}
+
+}
---
+++ proyecto_creadores/src/test/java/com/nfsm/springboot/web/app/ProyectoCreadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfsm.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCreadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
